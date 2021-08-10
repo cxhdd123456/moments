62927b08360baab0da30fce708edd95ce6cc03a4
Max Chen <56270582+cxhdd123456@users.noreply.github.com>
2021-08-10 18:49:57 -0400

    First Commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+import postRoutes from "./routes/posts.js"
+import userRoutes from "./routes/users.js"
+
+const app = express()
+dotenv.config()
+
+app.use(express.json({ limit: "30mb", extended: true}))
+app.use(express.urlencoded({ limit: "30mb", extended: true}))
+app.use(cors())
+
+app.use('/posts', postRoutes)
+app.use('/user', userRoutes)
+
+app.get('/', (req, res) => {
+  res.send("APP IS RUNNING")
+})
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
